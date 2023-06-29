33f5094eda7d1dcdbe686bb72b7327f6ad703355
Samy Samer <106012138+samySamer@users.noreply.github.com>
2023-06-29 15:24:09 +0200

    Revealing Sections Done

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -106,10 +106,8 @@ nav.addEventListener('mouseout', HoverHandler.bind(1));
 //IDEA New Implementation (GOOD)!
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
 const stickyNav = function (entries) {
   const entry = entries[0];
-  console.log(entry);
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
@@ -119,3 +117,22 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+/////////////////////////////////////////
+//Revealing Sections:
+
+const allSections = document.querySelectorAll('.section');
+const RevealFunction = function (entries, observer) {
+  const [entry] = entries;
+  //Guard clause
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const RevealingObserver = new IntersectionObserver(RevealFunction, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(section => {
+  RevealingObserver.observe(section);
+  section.classList.add('section--hidden');
+});
